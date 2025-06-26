7ca19756b6dae75d86aadd0afc92e078584fcc36
Keisuke-MARs <mtcs_drsp@icloud.com>
2025-06-26 13:50:39 +0900

    s3Upload.jsのコメント追加

--- src/utils/s3Upload.js
+++ src/utils/s3Upload.js
@@ -1,13 +1,18 @@
+// AWSのS3クライアント(S3を使うためのもの)をインポート
 import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
 
+// S3クライアントを作成
 const s3Client = new S3Client({
+  //AWSのリージョン
   region: process.env.REACT_APP_AWS_REGION,
+  //AWSのアクセスキー
   credentials: {
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
   }
 });
 
+//S3のバケット名
 const BUCKET_NAME = process.env.REACT_APP_AWS_S3_BUCKET_NAME;
 
 /**
@@ -16,14 +21,18 @@ const BUCKET_NAME = process.env.REACT_APP_AWS_S3_BUCKET_NAME;
  * @param {string} postId - 投稿ID（ファイル名の一部として使用）
  * @returns {Promise<string>} - S3の公開URL
  */
+//Base64形式の画像データと投稿IDを引数として渡す
+//instagramGraphAPI.jsから呼び出される
 export async function uploadImageToS3(base64Data, postId) {
   try {
-    // Base64データをBlobに変換
+    // Base64データをBlobに変換(データをバイナリデータに変換する)
     const response = await fetch(base64Data);
+    //結果からBlobを取得
     const blob = await response.blob();
     
-    // BlobをArrayBufferに変換してからUint8Arrayに
+    // BlobをArrayBuffer(バイナリデータを格納するためのJavaScriptのオブジェクト)に変換してからUint8Arrayに変換
     const arrayBuffer = await blob.arrayBuffer();
+    //8ビット符号なし整数の配列としてarrayBufferを変換する
     const uint8Array = new Uint8Array(arrayBuffer);
     
     // ユニークなファイル名を生成（postIdを含む）
@@ -37,12 +46,14 @@ export async function uploadImageToS3(base64Data, postId) {
       ContentType: 'image/jpeg'
     });
 
+    //S3クライアントにコマンドを送信
     await s3Client.send(command);
     
     // 公開URLを生成
     const publicUrl = `https://${BUCKET_NAME}.s3.${process.env.REACT_APP_AWS_REGION}.amazonaws.com/${fileName}`;
     
     console.log('S3アップロード成功:', publicUrl);
+    //公開URLを返す
     return publicUrl;
     
   } catch (error) {
@@ -55,21 +66,29 @@ export async function uploadImageToS3(base64Data, postId) {
  * S3からファイルを削除
  * @param {string} fileUrl - 削除するファイルのURL
  */
+//***********使われてないかも***********
+//S3からファイルを削除する関数
+//fileUrlは削除するファイルのURL
 export async function deleteFileFromS3(fileUrl) {
   try {
     // URLからファイルキーを抽出
     const url = new URL(fileUrl);
-    const fileName = url.pathname.substring(1); // 先頭の '/' を削除
+    //先頭の '/' を削除
+    const fileName = url.pathname.substring(1);
 
+    //S3クライアントにコマンドを送信
     const command = new DeleteObjectCommand({
       Bucket: BUCKET_NAME,
       Key: fileName
     });
 
+    //S3クライアントにコマンドを送信
     await s3Client.send(command);
+    //コンソールにログを出力
     console.log('S3ファイル削除成功:', fileName);
 
   } catch (error) {
+    //コンソールにエラーを出力
     console.error('S3ファイル削除エラー:', error);
   }
 }
